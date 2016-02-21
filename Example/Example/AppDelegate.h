b06187e710b4ee594f740ed37f2b17101cb82831
Jens Grud <jdgrud@gmail.com>
2016-02-21 16:21:06 +0100

    Adds example setup

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Jens Grud on 14/02/16.
+//  Copyright © 2016 Jens Grud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Jens Grud on 14/02/16.
+//  Copyright © 2016 Jens Grud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
